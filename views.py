3cf1357578c31c930ed26c7ccff2ac6eec02ccc8
hartnerk <hartnerk@gmail.com>
2021-03-23 09:05:17 -0700

    Task 3) Inout put above tags

--- views.py
+++ views.py
@@ -2,14 +2,7 @@ from flask import request, redirect
 from datetime import datetime
 from app import app
 from tag import Tag
-
-@app.route('/', methods=['GET'])
-def show_tags():
-    tags = Tag.select()
-    tags_html = '\n'.join(list(map(lambda x: x.name + "<br>", tags)))
-    form_html = "<form action=\"/tags\" method=\"POST\"><label>Enter a tag: </label><input name=\"tag-name\"></form>"
-    #embed()
-    return "<h1>The Ultimate Tag Manager</h1><h1>Hello World!</h1><img src=\"%s\" style=\"width:300px\"><div>%s</div><div>%s</div>" % (app.config['config']['awesome_image'],tags_html, form_html)
+#follow along update
 
 @app.route('/tags', methods=['POST'])
 def add_tag():
@@ -18,3 +11,12 @@ def add_tag():
       defaults={'created_at': datetime.now(), 'updated_at': datetime.now()})
 
     return redirect('/')
+    
+@app.route('/', methods=['GET'])
+def show_tags():
+    tags = Tag.select()
+    tags_html = '\n'.join(list(map(lambda x: x.name + "<br>", tags)))
+    form_html = "<form action=\"/tags\" method=\"POST\"><label>Enter a tag: </label><input name=\"tag-name\"></form>"
+    #embed()
+    return "<h1>The Ultimate Tag Manager</h1><h1>Hello World!</h1><img src=\"%s\" style=\"width:300px\"><div>%s</div><div>%s</div>" % (app.config['config']['awesome_image'], form_html, tags_html)
+
